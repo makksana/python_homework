d8db189186dbb1d4ff91b16c2e2fc1a5de9ed778
Oksana Yershova <makksana@gmail.com>
2021-12-03 20:20:36 +0200

    HW7 task 1

---
+++ Homework_7/HW7_1.py
@@ -0,0 +1,13 @@
+print('Task 1', end='\n\n')
+# A simple function.
+
+# Create a simple function called favorite_movie, 
+# which takes a string containing 
+# the name of your favorite movie. 
+# The function should then print 
+# “My favorite movie is named {name}”.
+
+def favorite_movie(name):
+    print('My favorite movie is named %s'%(name))
+
+favorite_movie('Lord of the Rings')
