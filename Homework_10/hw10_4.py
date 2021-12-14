2b0656358fe48ef0ee283803435fe622a5669f40
Oksana Yershova <makksana@gmail.com>
2021-12-14 18:47:50 +0200

    hw10 task 4 without comments

--- Homework_10/hw10_4.py
+++ Homework_10/hw10_4.py
@@ -5,20 +5,6 @@ print('Task 4', end='\n\n')
 # для хранения имени name и телефона phone. 
 # Обратите внимание, у друга может не быть телефона.
 
-# class Friend:
-#     # todo: ваш код тут
-#     pass
-
-# u1 = Friend("Андрей", "+380331233333")
-# u2 = Friend("Петр", )
-
-# assert hasattr(u1, "name")
-# assert hasattr(u1, "phone")
-# assert u1.name == "Андрей"
-# assert u2.name == "Петр"
-# assert u1.phone == "+380331233333"
-# assert u2.phone == ""
-
 class Friend:
     
     def __init__(self, name, phone=''):
