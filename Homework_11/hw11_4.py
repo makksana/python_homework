8c73e92e33f9c689057b8ee8e1f3ea64c241554e
Oksana Yershova <makksana@gmail.com>
2022-01-08 18:36:54 +0200

    hw11 task4

---
+++ Homework_11/hw11_4.py
@@ -0,0 +1,19 @@
+print('Task 4', end='\n\n')
+# Custom exception
+
+# Create your custom exception named `CustomException`, 
+# you can inherit from base Exception class, 
+# but extend its functionality to log every error message 
+# to a file named `logs.txt`. 
+# Tips: 
+# Use __init__ method to extend functionality 
+# for saving messages to file
+
+
+class CustomException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+        with open('logs.txt','a') as log_file:
+            log_file.write(msg + '\n')
+
+raise CustomException('One more log record2')
